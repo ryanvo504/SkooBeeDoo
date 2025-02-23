1547e4e6ba7deebbadbbeeea01dcb2945f2dde6d
ryanvo504 <ryanvo@ucsb.edu>
2025-02-22 19:58:16 -0800

    railway friendly db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,5 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import { readFileSync } from 'fs'; // Add this import at the top
 
 dotenv.config();
 
@@ -8,14 +7,20 @@ let db;
 export async function connectDB() {
     if (!db) {
         try {
-            // Determine the path based on the environment
-            const serviceAccountPath = process.env.NODE_ENV === 'production'
-                ? '/etc/secrets/serviceAccountKey.json'
-                : './config/serviceAccountKey.json';
-
-            // Initialize Firestore
+            // Initialize Firestore with environment variables
             admin.initializeApp({
-                credential: admin.credential.cert(serviceAccountPath)
+                credential: admin.credential.cert({
+                    "type": process.env.FIREBASE_TYPE,
+                    "project_id": process.env.FIREBASE_PROJECT_ID,
+                    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+                    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+                    "client_id": process.env.FIREBASE_CLIENT_ID,
+                    "auth_uri": process.env.FIREBASE_AUTH_URI,
+                    "token_uri": process.env.FIREBASE_TOKEN_URI,
+                    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+                    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+                })
             });
 
             db = admin.firestore(); // Create a Firestore instance
@@ -27,4 +32,3 @@ export async function connectDB() {
     }
     return db; // Return the existing Firestore instance
 }
-
