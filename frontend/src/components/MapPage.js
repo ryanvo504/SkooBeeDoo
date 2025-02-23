340ed6d796b7588e62be7794cd16507786681506
ryanvo504 <ryanvo@ucsb.edu>
2025-02-22 16:04:09 -0800

    hard code cities

--- frontend/src/components/MapPage.js
+++ frontend/src/components/MapPage.js
@@ -19,31 +19,31 @@ const CITY_COORDINATES = {
   
   // Additional Cities
   'Austin': { lat: 30.2672, lng: -97.7431 },
-  'Jacksonville': { lat: 30.3322, lng: -81.6557 },
   'San Francisco': { lat: 37.7749, lng: -122.4194 },
   'Columbus': { lat: 39.9612, lng: -82.9988 },
-  'Fort Worth': { lat: 32.7555, lng: -97.3308 },
   'Indianapolis': { lat: 39.7684, lng: -86.1581 },
   'Charlotte': { lat: 35.2271, lng: -80.8431 },
   'Seattle': { lat: 47.6062, lng: -122.3321 },
   'Denver': { lat: 39.7392, lng: -104.9903 },
-  'Washington D.C.': { lat: 38.9072, lng: -77.0369 },
   
   // More Cities
   'Boston': { lat: 42.3601, lng: -71.0589 },
   'El Paso': { lat: 31.7619, lng: -106.4850 },
   'Detroit': { lat: 42.3314, lng: -83.0458 },
-  'Nashville': { lat: 36.1627, lng: -86.7816 },
   'Portland': { lat: 45.5155, lng: -122.6789 },
-  'Memphis': { lat: 35.1495, lng: -90.0490 },
   'Oklahoma City': { lat: 35.4676, lng: -97.5164 },
   'Las Vegas': { lat: 36.1699, lng: -115.1398 },
   'Louisville': { lat: 38.2527, lng: -85.7585 },
   'Baltimore': { lat: 39.2904, lng: -76.6122 },
   'Milwaukee': { lat: 43.0389, lng: -87.9065 },
-  'Albuquerque': { lat: 35.0844, lng: -106.6504 },
   'Tucson': { lat: 32.2226, lng: -110.9747 },
-  'Fresno': { lat: 36.7378, lng: -119.7871 }
+  'Cleveland': { lat: 41.4993, lng: -81.6944},
+  'Kansas City': { lat: 39.0997, lng: -94.5786},
+  'Long Beach': { lat: 33.7701, lng: -118.1937},
+  'Memphis': { lat: 35.1495, lng: -90.0490},
+  'Minneapolis': { lat: 44.9778, lng: -93.2650},
+  'Oakland': { lat: 37.8044, lng: -122.2712},
+  
 };
 
 // Custom marker icons
