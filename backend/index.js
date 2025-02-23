479175884d770184b76acb603ee6c8df36b00555
ryanvo504 <ryanvo@ucsb.edu>
2025-02-22 21:32:24 -0800

    asdfasdf

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
             "http://localhost:3001",
             "http://localhost:3000",
             "http://localhost:5001",
-            "https://skoo-bee-doo.vercel.app/"], // Allow frontend URLs
+            "https://skoo-bee-doo.vercel.app"], // Allow frontend URLs
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 
