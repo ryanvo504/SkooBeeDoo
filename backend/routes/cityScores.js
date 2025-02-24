007352dd1086bed9ba74485312ff9ff62c644d50
ryanvo504 <ryanvo@ucsb.edu>
2025-02-24 14:56:53 -0800

    delete old backend

--- backend/routes/cityScores.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getCityScores } from '../controllers/cityScoresController.js';
-
-const router = express.Router();
-
-router.get('/city-scores', getCityScores);
-router.post('/city-scores', getCityScores);
-
-export default router;
